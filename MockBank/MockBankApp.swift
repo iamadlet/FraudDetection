59cfd1827ecb30ad0b6a1a4f09853390e1e28e9b
iamadlet <adlet.zhumagaliev2003@gmail.com>
2025-11-24 19:22:00 +0500

    Initial Commit

---
+++ MockBank/MockBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockBankApp.swift
+//  MockBank
+//
+//  Created by Адлет Жумагалиев on 24.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct MockBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
